aa1106ce8889c5d67c073abd333c1c772172fc0b
Aksh Kapoor <122724896+kapoor-Ak@users.noreply.github.com>
2025-08-05 22:17:26 +0100

    Update day 2.R

--- 2024/day 2.R
+++ 2024/day 2.R
@@ -8,6 +8,7 @@ input <- map(raw_input,\(x) str_split(x," ") |> unlist()|>  as.numeric())
 safe_flag <- function(report){
   #adjacent diffs
   Diff <- map(seq(1,length(report)-1),\(x) report[x+1]-report[x]) |> unlist() #Could have used seq_along here instead of seq
+  # or just could have used the normal diff() function
   #monotonous check
   Monotonous_Flag <- all(sign(Diff)==1) | all(sign(Diff)==-1)
   
@@ -16,6 +17,11 @@ safe_flag <- function(report){
   return(Monotonous_Flag*Adj_Flag)
 }
 
+part2_func <- function(report){
+  any(map(seq_along(report),\(x) report[-x] |> safe_flag()) ==1)
+}
+
 part_1 <- map(input[!is.na(input)],\(x) safe_flag(x)) |> unlist() |> sum()
 #606
-
+part_2 <-map(input[!is.na(input)],\(x) part2_func(x)) |> unlist() |> sum()
+#644
