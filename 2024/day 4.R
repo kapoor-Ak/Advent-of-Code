19e98305243d89bd22bc8dd4b60105cc6ce2d635
Aksh Kapoor <122724896+kapoor-Ak@users.noreply.github.com>
2025-08-12 23:24:57 +0100

    Update day 4.R

--- 2024/day 4.R
+++ 2024/day 4.R
@@ -15,7 +15,7 @@ bwd <- function(df){#\
   map(idx,\(x) df[x]|> str_flatten()|> str_extract_all(pattern=c("XMAS","SAMX")))|> unlist() |>na.omit()|> length()
       }
 fwd <- function(df){ df |> apply(2,rev)|> t() |> bwd()}
-#part2
+#part2 The current way is slow as it creates all possible 3x3 and checks it, may be able to find a better way in future
 X_mas <- function(df){
   all(c(any(diag(df) |> str_flatten() |>  str_detect(,pattern=c("MAS","SAM"))==1),
         any(df |> t() |> apply(2,rev) |> diag()|> str_flatten() |> str_detect(,pattern=c("MAS","SAM"))==1))==1)
@@ -24,7 +24,8 @@ part2_func <- function(df){
   positions <- expand.grid(row = 1:(nrow(input)-2), col = 1:(ncol(input)-2))
   sub <- pmap(positions, function(row, col) input[row:(row+2), col:(col+2)])
   map(seq_along(sub),\(x) sub[[x]] |> X_mas()) |> unlist() |> sum()
-}
+} 
 
 part1 <- horizontal(input)+vertical(input)+fwd(input)+bwd(input) #2496
 part2 <- part2_func(input) #1967
+
