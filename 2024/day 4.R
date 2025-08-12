b8cc22c1ab8aea4ebf2e1201af5d9927059c6645
Aksh Kapoor <122724896+kapoor-Ak@users.noreply.github.com>
2025-08-12 22:17:48 +0100

    Update day 4.R

--- 2024/day 4.R
+++ 2024/day 4.R
@@ -5,10 +5,6 @@ raw_input <- get_aoc(4) |> str_split("\n") |> pluck(1) |> head(-1)
 
 temp <- raw_input |> str_split("") |> unlist() |> matrix(nrow = length(raw_input),byrow = TRUE) # matrix creation works 
 
-pattern1 <- "XMAS"
-pattern2 <- "SAMX"
-pattern_use <- paste0("(", pattern1, "|", pattern2, ")")
-
 horizontal <-  function(df) {
   map(1:(nrow(df)),\(x) df[x,] |> str_flatten() |> str_extract_all(,pattern=c("XMAS","SAMX"))) |> unlist() |> length()}
 
@@ -17,12 +13,13 @@ vertical <- function(df){
 }
 bwd <- function(df){#\
   idx <- map(-nrow(df):nrow(df),\(x) which(row(df) - col(df) == x, arr.ind = TRUE))
-  map(idx,\(x) df[x]|> str_flatten()|> str_extract_all(pattern=c(pattern1,pattern2)))|> unlist() |>na.omit()|> length()
+  map(idx,\(x) df[x]|> str_flatten()|> str_extract_all(pattern=c("XMAS","SAMX"))|> unlist() |>na.omit()|> length()
       }
 fwd <- function(df){
   df |> apply(2,rev)|> t() |> bwd()
 }
 
 part1 <- horizontal(temp)+vertical(temp)+fwd(temp)+bwd(temp) #2496
+
 
 
